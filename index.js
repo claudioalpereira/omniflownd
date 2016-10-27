ac1bd94e55678f5d4f88d97213e0788a1773b3d7
Pereira <claudio.pereira@siemens.com>
2016-10-27 18:52:46 +0100

    commit 4

--- index.js
+++ index.js
@@ -17,4 +17,28 @@ app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
+io.on('connection', function(socket){
+      
+    //client.connect(19000, 'omniosaka.dyndns.org', function() {
+    client.connect(19000, '62.28.231.130', function() {
+        console.log('Connected');
+        client.write('+login[357976063980593 , Siemens , Omni2016]');
+		
+		 console.log("url"+socket.handshake.url);
+    clientId=socket.handshake.query.clientId;
+    console.log("connected clientId:"+clientId);
+    });
+        
+    client.on('data', function(data) {
+        console.log('Received: ' + data);
+        io.emit('result received', data.toString());
+    });
+        
+    socket.on('send cmd', function(msg){
+        console.log("Send Cmd:"+msg);
+        client.write(msg);
+    });
+});
+
+
 
